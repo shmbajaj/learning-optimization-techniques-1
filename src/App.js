91669899facac5d128fd7a0f7a3c39b3d3dea3ba
ShubhamBajaj <shmbajaj32@gmail.com>
2022-07-24 23:51:59 +0530

    refactor: removed unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header, Pagination } from "components";
+import { Home } from "pages";
+import { CardsProvider } from "context";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CardsProvider>
+      <div className="text-white bg-neutral-800 ">
+        <Header />
+        <Home />
+        <Pagination />
+      </div>
+    </CardsProvider>
   );
 }
-
-export default App;
